c7bb90e945635b5e5549c4cdc369e0864df7a7d5
amirparvaneh <amirparvaneh138@gmail.com>
2023-03-20 19:04:39 +0330

    dev-v1

---
+++ src/test/java/com/kazaar/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazaar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
